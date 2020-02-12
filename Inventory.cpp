e9f3074e4d70157a92b4983d49d90dc46e7b3711
Shelley Wong <shelleywong@wlan-t-250-5-dhcp.CSUChico.EDU>
2020-02-12 11:22:26 -0800

    Adds description of each Inventory class function

--- Inventory.cpp
+++ Inventory.cpp
@@ -1,3 +1,9 @@
+/*
+ * @file Inventory.cpp
+ * @author Shelley Wong
+ * 
+ * Functions for Inventory class.
+ */
 #include <iostream>
 #include <iomanip>
 #include "Inventory.h"
@@ -5,6 +11,12 @@
 using std::string;
 using std::ostream;
   
+/*
+ * @brief         Inventory constructor
+ * @param name    String name of the item in inventory
+ * @param price   Float price of item in inventory
+ * @param count   Integer count of how much of the item is in inventory
+ */
 Inventory::Inventory(string name, float price, int count)
 {
   m_name = name;
@@ -12,11 +24,25 @@ Inventory::Inventory(string name, float price, int count)
   m_in_stock = count;
 }
 
+/*
+ * @brief         Sell an item from inventory
+ *
+ * When an item from inventory is sold, decrease the number of
+ * items in stock.
+ */
 void Inventory::sell()
 {
   m_in_stock--;
 }
 
+/*
+ * @brief         Print the name and price of item in inventory
+ * @param stream  output stream for printing
+ * @param item    Item from inventory class
+ * @return        Return the output stream message
+ * 
+ * Print the item name and dollar price of the item.
+ */
 ostream& operator<<(ostream& stream, const Inventory& item)
 {
   stream << item.m_name 
